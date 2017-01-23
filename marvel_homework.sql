fac02e86b5741c9b9434959c0eae7d7d6713c4e0
3ll3n <ellengrafton01@gmail.com>
2017-01-23 16:46:50 +0000

    Completed Marvel homework questions

---
+++ marvel_homework.sql
@@ -0,0 +1,25 @@
+SELECT * FROM movies;
+
+SELECT name FROM people;
+
+UPDATE people SET name = 'Alastair Hood' WHERE name = 'Allisder Hood';
+-- SELECT name FROM people;
+
+SELECT name FROM people WHERE name = 'Ellen Grafton';
+
+DELETE FROM movies WHERE title = 'Batman Begins';
+-- SELECT * FROM movies;
+
+INSERT INTO people (name) VALUES ('John McCollum');
+-- SELECT name FROM people;
+
+DELETE FROM people WHERE name = 'Harvey Wheaton';
+-- SELECT name FROM people;
+
+INSERT INTO movies (title, year, show_time) VALUES ('Guardians of the Galaxy2', 2017, '24.00');
+-- SELECT * FROM movies;
+
+UPDATE movies SET show_time = '21.30' WHERE title = 'Guardians of the Galaxy';
+-- SELECT * FROM movies;
+
+
